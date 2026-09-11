5c04f1bb100512e28975d96ae4f0524c46a041c6
Ishaan Satapathy <ishaan@evolvex.io>
2026-09-11 21:02:28 +0530

    style(ui): improve Highlight corner notch pins for dark and light modes

--- src/components/ui/highlight.tsx
+++ src/components/ui/highlight.tsx
@@ -1,13 +1,19 @@
 import React from 'react'
 
-export function Highlight({ children }: { children: React.ReactNode }) {
+export function Highlight({
+  children,
+  className = '',
+}: {
+  children: React.ReactNode
+  className?: string
+}) {
   return (
-    <span className="relative inline-block rounded-[2px] border border-white/14 bg-white/[0.015] px-[0.055em] pb-[0.01em]">
+    <span className={`relative inline-block rounded-[3px] border border-foreground/15 dark:border-white/20 bg-foreground/[0.02] dark:bg-white/[0.025] px-1.5 py-0.5 font-medium transition-colors ${className}`}>
       {['-left-1 -top-1', '-right-1 -top-1', '-bottom-1 -left-1', '-bottom-1 -right-1'].map(
         (pos) => (
           <span
             key={pos}
-            className={`absolute ${pos} size-1 rounded-[1px] border border-white/25 bg-black`}
+            className={`absolute ${pos} size-1 rounded-[1px] border border-foreground/30 dark:border-white/35 bg-background dark:bg-black pointer-events-none`}
           />
         )
       )}
